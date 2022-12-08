f2c4e9a7cccaf1b7a8f323d9f59397e18e51301a
Akintolure Sheryl <sherylfadekemi@gmail.com>
2022-12-08 14:37:45 -0800

    4nn5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**main - print string using printf
- *
  * Return: 0
  */
 int main(void)
 {
-         printf("with proper grammar, but the outcome is a piece of art,");
-	 return 0;
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
 }
