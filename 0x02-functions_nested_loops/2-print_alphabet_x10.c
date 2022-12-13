b7e534e23b86120050be0325e0ee56f2311dbb73
Akintolure Sheryl <sherylfadekemi@gmail.com>
2022-12-13 11:35:13 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	int alpha = 0;
+
+	char = letter;
+
+	while (alpha++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
