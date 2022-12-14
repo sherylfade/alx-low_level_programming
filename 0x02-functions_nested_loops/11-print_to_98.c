e50fb991b37f25b77b985e74a4c9820c88b7349b
Akintolure Sheryl <sherylfadekemi@gmail.com>
2022-12-13 16:53:45 -0800

    task 11.0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,44 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
+		while (n >= 98)
+		{
+			if (n >= 100)
+				_putchar((n / 100) + '0');
+			_putchar(((n / 10) % 10) + '0');
+			_putchar((n % 10) + '0');
+
+			if (n == 98)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			n--;
+		}
 		while (n > 98)
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
+
 	else
 	{
+		while (n <= 98)
+		{
+			if (n < 0)
+				_putchar('-');
+
+			if (abs(n) >= 10)
+				_putchar((abs(n) / 10) + '0');
+			_putchar((abs(n) % 10) + '0');
+
+			if (n == 98)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
 		while (n < 98)
 			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
+	_putchar('\n');
 }
