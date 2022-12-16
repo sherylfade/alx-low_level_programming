5c41207161e2edda535191c4e727a8bcbfc18e8b
Akintolure Sheryl <sherylfadekemi@gmail.com>
2022-12-16 14:54:16 -0800

    function nested

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * @a: first int
+ * @b: second int
+ * Return: return of multipluication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
