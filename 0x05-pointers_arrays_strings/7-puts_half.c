0ba69d977ce9fdc80e4f492f2e4b2ac212c5b279
Akintolure Sheryl <sherylfadekemi@gmail.com>
2022-12-19 19:33:26 -0800

    last4

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half -  prints half of a string, followed by a new line
+ * @str: string to be printed
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[1] != '\0'; i++)
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
